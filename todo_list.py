f45b8b18cf85b34cc9e158eb7e3ec08538ed5895
Khushie Juneja <78829478+khushiej@users.noreply.github.com>
2021-02-18 15:02:50 +1300

    Add files via upload

--- todo_list.py
+++ todo_list.py
@@ -1,13 +1,16 @@
-print("Test")
+name = input("What is your name?")
+print ("Hi, {}".format(name))
+print("Welcome to your To Do List")
 
-def menu(): 
+def menu():
     mode = input("""1. Add item to your to do list, 2. Review your to do list, 3. Exit""")
     return mode
 
 def item_add():
     while True:
-        task_input = input("Enter a task or type end: ").strip() 
+        task_input = input("Enter a task or type end: ").strip()
         if task_input == "end":
+            print("Goodbye")
             break
         else:
             todo_list.append(task_input)
@@ -20,14 +23,15 @@ todo_list = []
 
 while True:
     user_input = menu()
-    if user_input == 1:
-        item_add() 
-    elif user_input == 2:
-        review() 
-    elif user_input == 3:
+    if user_input == "1":
+        item_add()
+    elif user_input == "2":
+        review()
+    elif user_input == "3":
+        print("Goodbye")
         break
-    else: 
+    else:
         print("Please enter item for your to do list or type end")
+       
+
         
-menu()
-   
